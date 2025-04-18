0e31dc9da91d79e7b1725d47a2f43ac3fb1595b2
Arthur Zaripov <a.zaripov@cyrm.ru>
2025-04-18 14:08:22 +0500

    F! - added patternTree

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"edaApp/pkg/patternTree"
+	"fmt"
+)
+
+func main() {
+
+	patterns := []string{
+		"market.*.data",
+		"market.*.data.**",
+		"user.login.*",
+		"service.*.status",
+		"**.critical",
+	}
+
+	events := []string{
+		"market.BTC.data",
+		"market.ETH.data.volume",
+		"user.login.success",
+		"service.web.status",
+		"app.service.",
+		"app.critical",
+	}
+
+	tree := patternTree.NewPatternTree()
+	for _, pattern := range patterns {
+		tree.AddPattern(pattern)
+	}
+
+	for i := 0; i < len(events); i++ {
+		event := events[i%len(events)]
+		fmt.Println(fmt.Sprintf("%s: %b", event, tree.Matches(event)))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module edaApp
+
+go 1.23.5
